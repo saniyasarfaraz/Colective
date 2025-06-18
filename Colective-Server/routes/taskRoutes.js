b2451ab13c3d130e1235949f32ada9efc2d2e24d
saniyasarfaraz <saniyasarfaraz61@gmail.com>
2025-06-18 14:36:57 +0530

    Fix Deployment Error

--- Colective-Server/routes/taskRoutes.js
+++ Colective-Server/routes/taskRoutes.js
@@ -1,20 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const taskController = require('../controllers/TasksController');
+const taskController = require("../controllers/tasksController");
 
 // Route to create a new task
-router.post('/', taskController.createTask);
+router.post("/", taskController.createTask);
 
 // Route to fetch tasks for a specific project
-router.get('/project/:projectId', taskController.getTasksByProject);
+router.get("/project/:projectId", taskController.getTasksByProject);
 
 // Route to fetch tasks assigned to a specific user
-router.get('/user/:userId', taskController.getTasksByUser);
+router.get("/user/:userId", taskController.getTasksByUser);
 
 // Route to edit a task
-router.patch('/:taskId', taskController.editTask);
+router.patch("/:taskId", taskController.editTask);
 
 // Route to delete a task
-router.delete('/:taskId', taskController.deleteTask);
+router.delete("/:taskId", taskController.deleteTask);
 
 module.exports = router;

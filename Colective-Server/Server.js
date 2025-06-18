a846b5adab51c4bf8f14862662baf30c30b29327
saniyasarfaraz <saniyasarfaraz61@gmail.com>
2025-06-18 14:16:41 +0530

    Deploying project

--- Colective-Server/Server.js
+++ Colective-Server/Server.js
@@ -1,46 +1,44 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const connectDB = require('./Config/db');  
-const path = require('path');
-const http = require('http');
-const { initSocket } = require('./socket');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./Config/db");
+const path = require("path");
+const http = require("http");
+const { initSocket } = require("./socket");
 
 dotenv.config();
 connectDB();
 const app = express();
 
 const server = http.createServer(app);
-initSocket(server); 
+initSocket(server);
 
 app.use(cors());
 app.use(express.json());
 
 // Serve static files from the 'uploads' directory
- 
-app.use('/overview', require('./routes/DashboardRoutes'));
-app.use('/profile', require('./routes/ProfileRoutes'));
 
-app.use('/auth', require('./routes/AuthRoutes'));
+app.use("/overview", require("./routes/dashboardRoutes"));
+app.use("/profile", require("./routes/ProfileRoutes"));
 
-app.use('/admin-projects', require('./routes/AdminProjectRoutes'));
+app.use("/auth", require("./routes/AuthRoutes"));
 
-app.use('/manageusers', require('./routes/AddUsersRoutes'));
+app.use("/admin-projects", require("./routes/AdminProjectRoutes"));
 
-app.use('/joinedprojects', require('./routes/JoinedProjectRoutes'));
+app.use("/manageusers", require("./routes/AddUsersRoutes"));
 
-app.use('/manageTasks', require('./routes/TaskRoutes'));
+app.use("/joinedprojects", require("./routes/JoinedProjectRoutes"));
 
-app.use('/projecttasks', require('./routes/UsersProjectTasksRoutes'));
+app.use("/manageTasks", require("./routes/TaskRoutes"));
 
-app.use('/comments', require('./routes/CommentsRoutes'));
+app.use("/projecttasks", require("./routes/UsersProjectTasksRoutes"));
 
-app.use('/projects', require('./routes/ProjectRoutes'));
+app.use("/comments", require("./routes/CommentsRoutes"));
 
+app.use("/projects", require("./routes/ProjectRoutes"));
 
 // will return a single task also
-app.use('/project-tasks', require('./routes/ProjectTasksRoutes'));
- 
+app.use("/project-tasks", require("./routes/ProjectTasksRoutes"));
 
 const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => console.log(`Scoket Server running on port ${PORT}`));
